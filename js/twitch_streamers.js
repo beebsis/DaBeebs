a69e04fe20fca580a3c6cdc22cd39e3cd1ee2a71
Beebsis <65894260+beebsis@users.noreply.github.com>
2022-10-04 15:32:45 +0200

    major

---
+++ js/twitch_streamers.js
@@ -0,0 +1,30 @@
+const streamers = 
+[
+    {
+        name: "",
+        link: "",
+        info: "",
+        pegi: {
+            adult: 
+                {
+                    age: "Adult",
+                    adult_icon: "fa-solid fa-person"
+                },
+            minor:
+                {
+                    age: "Minor",
+                    minor_icon: "fa-solid fa-child"
+                }
+        },
+        picture: "",
+    },
+]
+
+function createStreamer() {
+
+}
+
+
+createStreamer;
+
+console.log(createStreamer);
